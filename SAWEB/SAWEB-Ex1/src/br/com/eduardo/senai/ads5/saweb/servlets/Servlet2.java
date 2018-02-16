75e7343b062c21da739a77df29d378f55448ba08
Eduardo Macedo Santos <dudmacedo@gmail.com>
2018-02-16 20:27:36 -0200

    Incluído Exercício 01 - Disciplina SAWEB

---
+++ SAWEB/SAWEB-Ex1/src/br/com/eduardo/senai/ads5/saweb/servlets/Servlet2.java
@@ -0,0 +1,60 @@
+package br.com.eduardo.senai.ads5.saweb.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Servlet2
+ */
+@WebServlet("/Servlet2")
+public class Servlet2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public Servlet2() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	private void doResponse(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setCharacterEncoding("UTF-8");
+		RequestDispatcher view = request.getRequestDispatcher("response.jsp");
+		view.forward(request, response);
+		// response.set
+
+		// response.getWriter().append("Olá
+		// ").append(request.getParameter("nome")).append("
+		// ").append(request.getParameter("sobrenome"));
+
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doResponse(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doResponse(request, response);
+	}
+
+}
